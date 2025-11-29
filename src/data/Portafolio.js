e7be46fed40429e6b1934748444a0a6737b3878a
Tonyajca21 <Castroanthony1806@gmail.com>
2025-11-28 18:29:36 -0600

    feat:imagen de proyecto y cursos

---
+++ src/data/Portafolio.js
@@ -0,0 +1,52 @@
+
+
+export const perfil = [{
+    nombre: "Anthony Castro",
+    profesion: "Licenciado En Informatica En Administrativa",
+    descripcion: "Egresado de licenciatura en informática administrativa en la Universidad Nacional Autónoma De Honduras con conocimientos en desarrollo de aplicaciones web, especialmente en frontend, y habilidades en diseño gráfico. Apasionado por crear experiencias digitales atractivas y funcionales.",
+    ubicacion: "Tegucigalpa, Honduras",
+    email: "ajcasavila2006@gmail.com",
+    telefono: "+504 9956-1234",
+ 
+}]
+
+
+export const proyectos = [
+    {
+        titulo: "Concursos Publico UNAH",  
+        descripcion: "Plataforma web para la gestión de concursos públicos en la Universidad Nacional Autónoma de Honduras (UNAH). Permite a los usuarios registrarse, postularse a concursos y gestionar sus aplicaciones de manera eficiente.",  
+        imagen: "../../src/assets/ConcursosUnah.png",
+        url: "http://concursos-test.unah.edu.hn.s3-website-us-east-1.amazonaws.com/#/landing",
+        icons:{
+            tecnologia1: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+            tecnologia2: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+            tecnologia3: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+            tecnologia4: "https://raw.githubusercontent.com/devicons/devicon/master/icons/angular/angular-original.svg",
+            tecnologia5: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain.svg",
+            tecnologia6: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+            tecnologia7: "https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg",
+            tecnologia8: "https://raw.githubusercontent.com/devicons/devicon/master/icons/visualstudio/visualstudio-plain.svg",
+            tecnologia9: "https://raw.githubusercontent.com/devicons/devicon/master/icons/npm/npm-original-wordmark.svg",
+            tecnologia10: "https://raw.githubusercontent.com/devicons/devicon/master/icons/webpack/webpack-original.svg",
+            tecnologia11: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg",
+            tecnologia12: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+            tecnologia13: "https://raw.githubusercontent.com/devicons/devicon/master/icons/angularjs/angularjs-original.svg",
+            tecnologia14: "https://raw.githubusercontent.com/devicons/devicon/master/icons/jquery/jquery-original.svg",
+        }
+    
+    },
+    {
+     titulo: "Ecommerce - Tienda Virtual",
+    imagen: "../../src/assets/Ecommerse.png",
+  descripcion: "Proyecto en desarrollo de una tienda virtual, donde se implementan funcionalidades de catálogo de productos, carrito de compras, autenticación de usuarios y gestión de pedidos. El proyecto utiliza tecnologías modernas para ofrecer un rendimiento óptimo y una experiencia de usuario responsiva.",
+  icons: {
+    tecnologia1: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    tecnologia2: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vite/vite-original.svg",
+    tecnologia3: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+    tecnologia4: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+    tecnologia5: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-plain.svg"
+  },
+  estado: "En desarrollo",
+  url: "https://github.com/TonyAjca21/Proyecto-ECOMMERSEFront" // como no tiene link aún
+    }
+]
