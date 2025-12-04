7d9ba7b0355696c6845bd822c4020df33c79cc44
Tonyajca21 <Castroanthony1806@gmail.com>
2025-12-04 17:19:48 -0600

    fix:card animation , feat:exp

--- src/data/Portafolio.js
+++ src/data/Portafolio.js
@@ -25,13 +25,7 @@ export const proyectos = [
             tecnologia5: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain.svg",
             tecnologia6: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
             tecnologia7: "https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg",
-            tecnologia8: "https://raw.githubusercontent.com/devicons/devicon/master/icons/visualstudio/visualstudio-plain.svg",
-            tecnologia9: "https://raw.githubusercontent.com/devicons/devicon/master/icons/npm/npm-original-wordmark.svg",
-            tecnologia10: "https://raw.githubusercontent.com/devicons/devicon/master/icons/webpack/webpack-original.svg",
-            tecnologia11: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg",
-            tecnologia12: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
-            tecnologia13: "https://raw.githubusercontent.com/devicons/devicon/master/icons/angularjs/angularjs-original.svg",
-            tecnologia14: "https://raw.githubusercontent.com/devicons/devicon/master/icons/jquery/jquery-original.svg",
+           
         }
     
     },
@@ -50,3 +44,82 @@ export const proyectos = [
   url: "https://github.com/TonyAjca21/Proyecto-ECOMMERSEFront" // como no tiene link aún
     }
 ]
+
+export const experiencia = [
+      {
+        puesto: "Practica Profesional Licenciado en Informatica Administrativa",
+        descripcion: "Participación en el desarrollo y mantenimiento de sistemas informáticos internos, contribuyendo a la optimización de procesos administrativos mediante la implementación de soluciones tecnológicas efectivas.",
+        fechainicio: "Julio 2022",
+        fechafin: "Diciembre 2022",
+        empresa: "DEGT-UNAH",
+        ubicacion: "Tegucigalpa, Honduras",
+        actividades: [{
+            "Colaborar en el desarrollo y mantenimiento de sistemas informáticos internos.": "",
+            "Asistir en la implementación de soluciones tecnológicas para optimizar procesos administrativos.": "",
+            "Realizar pruebas y depuración de software para garantizar su correcto funcionamiento.": "",
+            "Documentar procedimientos y actualizaciones de sistemas.": "",
+            "Participar en reuniones de equipo para discutir avances y desafíos.": ""        
+        }]
+    },
+    {
+        puesto: "Beca por desempeño en Desarrollo Frontend",
+        descripcion: "Desarrollo y mantenimiento de la plataforma web de concursos públicos de la Universidad Nacional Autónoma de Honduras (UNAH) utilizando Angular. Implementación de nuevas funcionalidades, optimización del rendimiento y aseguramiento de la calidad del código mediante pruebas y revisiones.",
+        fechainicio: "Enero 2023",
+        fechafin: "Diciembre 2023",
+        empresa: "DEGT-UNAH",
+        ubicacion: "Tegucigalpa, Honduras",
+        actividades: [{
+            "Desarrollar y mantener la plataforma web de concursos públicos utilizando Angular.": "",
+            "Colaborar con el equipo de diseño para implementar interfaces de usuario atractivas y funcionales.": "",
+            "Optimizar el rendimiento de la aplicación para mejorar la experiencia del usuario.": "",
+            "Realizar pruebas y depuración para garantizar la calidad del código.": "",
+            "Participar en reuniones de equipo para discutir avances y planificar futuras mejoras.": ""
+        }]
+    }
+
+  ]
+
+  export const certificados = [
+  {
+    titulo: "Introducción a la Agilidad",
+    descripcion: "Descripción breve del certificado o curso. Lorem ipsum dolor sit amet.",
+    imagen: "../../src/assets/Introduccion a la agilidad.png",
+    archivo: "../../src/assets/diplomaAgilidad.pdf",
+    nombreDescarga: "diplomaAgilidad.pdf"
+  },
+  {
+    titulo: "Introducción a Scrum",
+    descripcion: "Descripción breve del certificado o curso. Lorem ipsum dolor sit amet.",
+    imagen: "../../src/assets/Introduccion Scrum.png",
+    archivo: "../../src/assets/diplomaScrum.pdf",
+    nombreDescarga: "diplomaScrum.pdf"
+  },
+  {
+    titulo: "Formulación y evaluación de proyectos",
+    descripcion: "Descripción breve del certificado o curso. Lorem ipsum dolor sit amet.",
+    imagen: "../../src/assets/formulacion y evalucion de proyecto.png",
+    archivo: "../../src/assets/Formulación y evaluación de proyectos.pdf",
+    nombreDescarga: "Formulación y evaluación de proyectos.pdf"
+  },
+  {
+    titulo: "Transformación digital y evolución tecnológica",
+    descripcion: "Descripción breve del certificado o curso. Lorem ipsum dolor sit amet.",
+    imagen: "../../src/assets/transformacion Digital.png",
+    archivo: "../../src/assets/Transformación digital y evolución tecnológica.pdf",
+    nombreDescarga: "Transformación digital y evolución tecnológica.pdf"
+  },
+  {
+    titulo: "Evaluador de procesos",
+    descripcion: "Descripción breve del certificado o curso. Lorem ipsum dolor sit amet.",
+    imagen: "../../src/assets/evaluador de procesos.png",
+    archivo: "../../src/assets/Evaluador de procesos.pdf",
+    nombreDescarga: "evaluador de procesos.pdf"
+  },
+  {
+    titulo: "High School Diploma",
+    descripcion: "Descripción breve del certificado o curso. Lorem ipsum dolor sit amet.",
+    imagen: "https://via.placeholder.com/400x200",
+    archivo: "../../src/assets/Evaluador de procesos.pdf",
+    nombreDescarga: "evaluador de procesos.pdf"
+  }
+];
